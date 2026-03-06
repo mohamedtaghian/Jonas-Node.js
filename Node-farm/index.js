437e1265b921f59f29028a34489c282b228dd660
mohamedtaghian <mohamedtaghian12@gmail.com>
2026-03-06 03:58:40 +0200

    feat added routing

---
+++ Node-farm/index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+// <---------------- SERVER ---------------->
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
